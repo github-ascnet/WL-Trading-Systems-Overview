4e612d45d40af12f7f0430ed71166cac528636f8
Sascha Affolter <sascha_affolter@hotmail.com>
2026-04-16 09:55:17 +0700

    Re-orderd Systems in Landing-Page

--- js/systems.js
+++ js/systems.js
@@ -8,20 +8,20 @@ const SYSTEMS = [
     dataPath: "./systems/short-burst-momentum",
   },
   {
-    id: "multifactor-edge",
-    dataPath: "./systems/multifactor-edge",
-  },
-  {
-    id: "capitulation-bounce",
-    dataPath: "./systems/capitulation-bounce",
-  },
-  {
     id: "monday-anchor-reversion",
     dataPath: "./systems/monday-anchor-reversion",
   },
   {
     id: "dip-snapback",
     dataPath: "./systems/dip-snapback",
+  },
+  {
+    id: "multifactor-edge",
+    dataPath: "./systems/multifactor-edge",
+  },
+  {
+    id: "capitulation-bounce",
+    dataPath: "./systems/capitulation-bounce",
   },
   {
     id: "bear-market-reversion",
